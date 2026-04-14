73c09ac4071f725e824329c322582dcab70b9923
osmarpetry <osmarpetry@gmail.com>
2026-04-14 14:53:39 +0200

    move .. the folder

---
+++ slc-next/.next/dev/static/chunks/[project]_pages__error_07xvfw~._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/0827_next_dist_compiled_0q-a-4m._.js","static/chunks/0827_next_dist_shared_lib_0~40e-w._.js","static/chunks/0827_next_dist_client_0fgghr-._.js","static/chunks/0827_next_dist_0rq-4rb._.js","static/chunks/10i1_next_dist_pages__error_10tf.vf.js","static/chunks/[next]_entry_page-loader_ts_0qhuauc._.js","static/chunks/0p2h_react-dom_0vsf22y._.js","static/chunks/node_modules__pnpm_13qx_gy._.js","static/chunks/[root-of-the-server]__01mw43t._.js"],
+    source: "entry"
+});
